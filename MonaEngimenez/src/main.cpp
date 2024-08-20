77acaf580f24a63d41e1633fee5a312786a1eabe
MateoMonastra <mateomonastravm@gmail.com>
2024-08-20 11:34:46 -0300

    Added Glew deleted files

--- MonaEngimenez/src/main.cpp
+++ MonaEngimenez/src/main.cpp
@@ -1,44 +1,55 @@
+#include <glew.h>
+#include <GLFW/glfw3.h>
+
 #include "Renderer/Renderer.h"
 #include "Window/Window.h"
 
-#include <GLFW/glfw3.h>
 
 
 int main(void)
 {
-    Window window;
-    Renderer renderer;
+	Window window;
+	Renderer renderer;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
-    window.Create(640, 480, "Hello World", NULL, NULL);
+	window.Create(640, 480, "Hello World", NULL, NULL);
 
-    window.SetCurrent();
+	window.SetCurrent();
 
-    /* Loop until the user closes the window */
-    while (!window.ShouldClose())
-    {
-        renderer.Clear(0.5f, 0.5f, 0.5f, 1.0f);
+	float positions[6]
+	{
+		-0.5f,-0.5f,
+		0.0f,0.5f,
+		0.5f,-0.5f
 
-        glBegin(GL_TRIANGLES);
+	};
 
-        glColor3f(0.5, 0, 0);
+		/* Loop until the user closes the window */
+		while (!window.ShouldClose())
+		{
+			renderer.Clear(0.5f, 0.5f, 0.5f, 1.0f);
 
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.5f, -0.5f);
+			glBegin(GL_TRIANGLES);
 
-        glEnd();
+			glColor3f(0.5, 0, 0);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window.GetWindow());
+			unsigned int buffer;
+			glGenBuffers(1, &buffer);
+			glBindBuffer(GL_ARRAY_BUFFER, buffer);
+			glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions,GL_STATIC_DRAW);
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+			glEnd();
 
-    glfwTerminate();
-    return 0;
+			/* Swap front and back buffers */
+			glfwSwapBuffers(window.GetWindow());
+
+			/* Poll for and process events */
+			glfwPollEvents();
+		}
+
+	glfwTerminate();
+	return 0;
 }
